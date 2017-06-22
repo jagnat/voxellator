ccd524354fb0f83cf7e142b7900b3ebadfe649fc
Jagannath Natarajan <jagnat4000@gmail.com>
2017-06-21 20:07:34 -0700

    Work on loading shaders

--- src/vox_gl_triangle.c
+++ src/vox_gl_triangle.c
@@ -1,6 +1,8 @@
 
 // OpenGL test code
 
+#include <stdio.h>
+
 uint createGlProgram(char *vertex, char *fragment);
 uint loadGlShader(char *filename);
 
@@ -19,8 +21,44 @@ void createGlProgram(char *vertex, char *fragment)
 
 }
 
-uint loadGlShader(char *filename)
+// TODO: Platform-specific file handling for hotloading
+uint loadGlShader(char *filename, ShaderType shaderType)
 {
+	uint shaderId;
+	FILE *shaderfile = fopen(filename, "r");
+	if (!shaderfile)
+		return 0;
+	
+	fseek(shaderfile, 0, SEEK_END);
+	int filesize = ftell(shaderfile);
+	rewind(shaderfile);
+
+	char *filebuf = malloc(filesize * sizeof(char));
+	if (fread(filebuf, 1, filesize, shaderfile) != filesize)
+		return 0;
+	
+	GLenum glShaderType = GL_VERTEX_SHADER;
+	if (shaderType == SHADER_FRAG)
+		glShaderType = GL_FRAGMENT_SHADER;
+	
+	shaderId = glCreateShader(glShaderType);
+	if (!shaderId)
+		return shaderId;
+
+	glShaderSource(shaderId, 1, &filebuf, 0);
+
+	glCompileShader(shaderId);
+	int status = 0;
+	glGetShaderiv(shaderId, GL_COMPILE_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &status);
+
+		char *buf = calloc(1, status * sizeof(char));
+
+		glGetShaderInfoLog(shaderId, status, 0, buf);
+		printf("GL shader error!!! \n\n%s\n", buf);
+	}
 
 }
 
--- src/vox_gl_triangle.h
+++ src/vox_gl_triangle.h
@@ -1,6 +1,12 @@
 #ifndef _VOX_GL_TRIANGLE_H_
 #define _VOX_GL_TRIANGLE_H_
 
+typedef enum
+{
+	SHADER_VERT,
+	SHADER_FRAG
+} ShaderType;
+
 void initGlTriangle();
 void drawGlTriangle();
 
