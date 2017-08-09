31184e65201458f8352b9f59889df087a67730a8
Jagannath Natarajan <jagnat4000@gmail.com>
2017-08-09 11:05:10 -0700

    Add simple phong shading, minus specular

--- win/fragment.glsl
+++ win/fragment.glsl
@@ -1,11 +1,23 @@
 #version 330 core
 
-in vec4 out_Color;
+in vec4 fragColor;
+in vec3 fragNormal;
+in vec3 fragPos;
 
-out vec4 final_Color;
+out vec4 finalColor;
+
+uniform vec3 lightPos = vec3(32, 70, 32);
 
 void main(void)
 {
+	float ambientStr = 0.5;
+	vec3 ambient = ambientStr * vec3(1, 1, 1);
+
+	vec3 lightDir = normalize(lightPos - fragPos);
+
+	float diff = max(dot(fragNormal, lightDir), 0);
+
+	vec3 diffuse = diff * vec3(1, 1, 1);
 	// Color
-	final_Color = out_Color;
+	finalColor = vec4((ambient + diffuse) * fragColor.xyz, 1);
 }
--- win/vertex.glsl
+++ win/vertex.glsl
@@ -2,15 +2,20 @@
 
 uniform mat4 projMatrix = mat4(1);
 uniform mat4 viewMatrix = mat4(1);
+uniform mat4 modelMatrix = mat4(1);
 
-layout(location = 0) in vec4 in_Position;
-layout(location = 1) in vec4 in_Color;
-layout(location = 2) in vec4 in_Normal;
+layout(location = 0) in vec4 inPosition;
+layout(location = 1) in vec4 inColor;
+layout(location = 2) in vec4 inNormal;
 
-out vec4 out_Color;
+out vec4 fragColor;
+out vec3 fragNormal;
+out vec3 fragPos;
 
 void main(void)
 {
-	gl_Position = (projMatrix * viewMatrix) * vec4(in_Position.xyz, 1);
-	out_Color = in_Color;
+	gl_Position = (projMatrix * viewMatrix * modelMatrix) * vec4(inPosition.xyz, 1);
+	fragColor = inColor;
+	fragNormal = inNormal.xyz;
+	fragPos = vec3(modelMatrix * vec4(inPosition.xyz, 1));
 }
