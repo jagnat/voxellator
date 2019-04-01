8e9fbc76618c2e100a2cd70f8e24b2c1c7de91cd
Jagannath Natarajan <jagnat4000@gmail.com>
2019-04-01 13:55:47 -0700

    Hacky solution to windows build

--- win/build.bat
+++ win/build.bat
@@ -5,8 +5,6 @@ set output=..\win\vox.exe
 set cflags=/nologo /MT /Zi /EHsc
 set libs=LIBCMT.lib User32.lib Gdi32.lib OpenGL32.lib
 set lflags=/OUT:%output% /SUBSYSTEM:CONSOLE /INCREMENTAL:NO
-set srcs=win32_vox.c vox_main.c vox_jobs.c vox_mesher.c vox_noise.c vox_render.c vox_world.c
+set srcs=..\src\win32_vox.c ..\src\vox_main.c ..\src\vox_jobs.c ..\src\vox_mesher.c ..\src\vox_noise.c ..\src\vox_render.c ..\src\vox_world.c
 
-pushd ..\src\
 cl %srcs% %cflags% /link %libs% %lflags%
-popd
